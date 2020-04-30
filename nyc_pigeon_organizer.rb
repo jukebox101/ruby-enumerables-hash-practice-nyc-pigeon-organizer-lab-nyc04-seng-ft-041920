83e149b90786d9bd45a015e69a9663fce075ca4c
Leonora Squires <leonora.squires@gmail.com>
2020-04-30 16:26:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |traits, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][key] == nil
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key].push(stats.to_s)
+      end
+    end 
+  end
+  pigeon_list
 end
